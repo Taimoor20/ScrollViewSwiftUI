5d3df508c0661583cc1c4cebe1430b00234bbde0
Taimoor20 <tamooararif20@gmail.com>
2022-05-16 13:33:04 +0500

    Initial Commit

---
+++ Scroll view/Scroll_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scroll_viewApp.swift
+//  Scroll view
+//
+//  Created by Taimoor Arif on 16/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Scroll_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
